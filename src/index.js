905d22f6ca3c343ae50e8e94904152f274b5692d
Solovev Pavel <p.solovev@elewise.com>
2019-08-20 19:59:34 +0400

    asset manegment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import _ from "lodash";
+import "./style.css";
+import Apple from "./apple.jpg";
+import Data from "./data.xml";
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add("hello");
 
+  const myIcon = new Image();
+  myIcon.src = Apple;
+  
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,49 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          "file-loader"
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          "csv-loader"
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          "xml-loader"
+        ]
+      }
+    ]
   }
 }
